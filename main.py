7e7e49ae1dda53c2713b9fdffa6b816443b5e96e
Guillermo1821 <juan.gui.18@hotmail.com>
2021-11-29 11:04:39 -0500

    validations_body

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ from fastapi import Body, Query, Path
 app = FastAPI()
 
 #Models
+
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -64,3 +71,22 @@ def show_person(
         )
 ):
     return {person_id: "It exists!"}
+
+
+#validaciones: request body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person ID",
+        description="This is the person ID",
+        gt=0
+    ),
+    person: Person = Body(...),
+    Location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(Location.dict())
+
+    return results
